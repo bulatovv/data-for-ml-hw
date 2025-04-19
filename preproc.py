0a7c2855a37ed83090d7830e0751ac0558c03e47
Albert Fazlyev <albert.fz@yandex.ru>
2025-04-19 10:59:47 +0500

    fix preproc basket_id collision

--- preproc.py
+++ preproc.py
@@ -13,9 +13,18 @@ fiscal_data = pl.scan_ndjson('storage/fiscal_data.jsonl.gz', infer_schema_length
 receipts = receipts.join(brands, left_on='brandId', right_on='id')
 receipts = receipts.join(fiscal_data, on='key')
 
+
+items_annotated = pl.scan_csv('storage/train.csv').select(
+    basket_id='check_id',
+    item='name',
+    item_type='category',
+    item_price='price',
+    item_quantity='count'
+)
+
 items = (
     receipts
-    .with_row_index('basket_id')
+    .with_row_index('basket_id', offset=items_annotated.count().collect().item(0, 0) + 1)
     .explode('items')
     .select(
         'basket_id',
@@ -26,14 +35,6 @@ items = (
         item_price=pl.col('items').struct.field('price'),
         item_quantity=pl.col('items').struct.field('quantity').cast(pl.Float64)
     )
-)
-
-items_annotated = pl.scan_csv('storage/train.csv').select(
-    basket_id='check_id',
-    item='name',
-    item_type='category',
-    item_price='price',
-    item_quantity='count'
 )
 
 pl.concat([items, items_annotated], how='diagonal_relaxed').sink_parquet('storage/items.parquet')
